856f3ec916acf29536dfd8986c91e7a257abac48
Ntuyenabo <entuyenabo@weathertop.middlebury.edu>
2024-11-12 17:45:01 -0500

    implemented calloc and realloc. Refactore/fixed bug in free()

--- my-malloc.c
+++ my-malloc.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 static void *bottom;
-static int extra = 10000;
+static int extra = 2;
 
 
 typedef struct my_struct {
@@ -25,7 +25,7 @@ void *malloc(size_t req_size) {
     while (current_record->next_record != current_record) {
         if (current_record->free == 1){
             // Check to see if the freed chunck is the right size
-            
+
             if (req_size <= current_record->section_size) {
                 current_record->free = 0;
                 //cast to char pointer for intuitive arithmatic
@@ -37,7 +37,7 @@ void *malloc(size_t req_size) {
 
     if (current_record->free == 1){
         // Check to see if the freed chunck is the right size
-        
+
         if (req_size <= current_record->section_size) {
             current_record->free = 0;
             //cast to char pointer for intuitive arithmatic
@@ -54,14 +54,71 @@ void *malloc(size_t req_size) {
 }
 
 void *calloc(size_t nmemb, size_t size) {
-    return NULL;
+
+    int round_size = (nmemb*size) + ((nmemb*size) % 16);
+    if(bottom == NULL){
+        bottom = sbrk(round_size + sizeof(heap_record) + extra);
+        heap_record *head = bottom;
+        head->next_record = bottom;
+        head->section_size = round_size;
+        //fill the requested chunk
+        void *addr = (char *) head + sizeof(heap_record);
+        memset(addr, 0, round_size);
+        head->free = 0;
+        return (char *) bottom + sizeof(heap_record);
+    }
+    heap_record *current_record = bottom;
+    void *addr = (char *) current_record + sizeof(heap_record);
+    while (current_record->next_record != current_record) {
+        if (current_record->free == 1){
+            // Check to see if the freed chunck is the right size
+
+            if (round_size <= current_record->section_size) {
+                current_record->free = 0;
+                memset(addr, 0, round_size);
+                return (char *) current_record + sizeof(heap_record);
+            }
+        }
+        current_record = current_record->next_record;
+    }
+
+    if (current_record->free == 1){
+        // Check to see if the freed chunck is the right size
+
+        if (round_size <= current_record->section_size) {
+            current_record->free = 0;
+            //cast to char pointer for intuitive arithmatic
+            memset(addr, 0, round_size);
+            return (char *) current_record + sizeof(heap_record);
+        }
+    }
+    // At this point, we have checked all existing records and have not found a free one that's big enough.
+    current_record->next_record = (char *) current_record + sizeof(heap_record) + current_record->section_size;
+    heap_record *new_record = current_record->next_record;
+    new_record->next_record = new_record;
+    new_record->section_size = round_size;
+    new_record->free = 0;
+    memset(addr, 0, round_size);
+    return (char *) new_record + sizeof(heap_record);
 }
 
 void *realloc(void *ptr, size_t size) {
-    return NULL;
+    // check if size is greater than previous allocation chunk
+    heap_record *record_to_reallocate = (void *)((char *) ptr - sizeof(heap_record));
+
+    if(record_to_reallocate->section_size < size) {
+        return NULL;
+    }
+    // reallocate requested chunk
+    record_to_reallocate->free = 0;
+    record_to_reallocate->section_size = 0;
+    return (char *) record_to_reallocate + sizeof(heap_record);
+
 }
 
 void free(void *ptr) {
-    heap_record *record_to_free = (void *)((char *) ptr - sizeof(heap_record));
-    record_to_free->free = 1;
+    if(ptr != NULL){
+        heap_record *record_to_free = (void *)((char *) ptr - sizeof(heap_record));
+        record_to_free->free = 1;
+    }
 }
--- test.c
+++ test.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void realloc_test();
+
 int main(int argc, char *argv[]){
+            
+    char str[] = "Esdras";
     void * p1 = malloc(10);
     void * p2 = malloc(10);
     free(p1);
     free(p2);
-    void * p3 = malloc(8);
-    void * p4 = malloc(8);
+    void * p3 = malloc(40);
+    char * p4 = (char *) malloc(8);
+
+    for (int i=0; i<6; i++) {
+        *(p4+i) = str[i];    
+    }
+    char *p5 = realloc(p4, 10);
+    free(p5);
+
+    char * p6 = (char *) calloc(8, sizeof(char));
+    free(p6);
 
     return 1;
 }
+
