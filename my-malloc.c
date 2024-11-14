30a822ebe37f903a076b8928915781104b253e41
Wade <gdwade@weathertop.middlebury.edu>
2024-11-14 14:56:22 -0500

    shorter realloc

--- my-malloc.c
+++ my-malloc.c
@@ -7,6 +7,8 @@ static void *bottom;
 static int extra_bytes = 10000;
 static void *top_of_heap;
 
+void free(void *ptr);
+
 typedef struct my_struct {
     void *next_record;
     unsigned int section_size;
@@ -46,7 +48,6 @@ void *malloc(size_t req_size) {
 
     if (current_record->free == 1){
         // Check to see if the freed chunck is the right size
-
         if (req_size <= current_record->section_size) {
             current_record->free = 0;
             //cast to char pointer for intuitive arithmatic
@@ -57,10 +58,10 @@ void *malloc(size_t req_size) {
     if ((void *) ((char *)current_record + round_size) >= (void *) top_of_heap){
         // We have reached the end of the heap, so we must extend it.
         if (brk((char *) top_of_heap + extra_bytes) == -1){
-            // Hqandle the error, but i dont really know what we'd do here. 
+            // Hqandle the error, but i dont really know what we'd do here.
+            return NULL; 
         }
     }
-
     current_record->next_record = (char *) current_record + sizeof(heap_record) + current_record->section_size;
     heap_record *new_record = current_record->next_record;
     new_record->next_record = new_record;
@@ -79,88 +80,19 @@ void *realloc(void *ptr, size_t size) {
         void * val = malloc(size);
         return val;
     }
+    if (size == 0) {
+        free(ptr);
+    }
     // check if size is greater than previous allocation chunk
     heap_record *record_to_reallocate = (void *)((char *) ptr - sizeof(heap_record));
-
-    if(record_to_reallocate->section_size < size) {
-
-        heap_record *current_record = bottom;
-        while (current_record->next_record != current_record) {
-            if (current_record->free == 1){
-                // Check to see if the freed chunck is the right size
-
-                if (size <= current_record->section_size) {
-                    //copy initialized data from record to reallocate
-                    memcpy(current_record, record_to_reallocate, sizeof(heap_record) + record_to_reallocate->section_size);
-
-                    int old_record_section_size = current_record->section_size;
-                    current_record->section_size = size;
-                    current_record->free = 0;
-
-                    if(current_record->next_record != current_record) {
-                        // try to see if we can fit a memory block between the reallocated block and the one after
-                        int space_remaining = (char *)current_record->next_record - ((char *)current_record + sizeof(heap_record)*2 + current_record->section_size);
-                        //check if a new block can be allocated within remaining space
-                        if(space_remaining >= 16) {
-                            //create new block within remaining space
-                            int new_size = space_remaining-(space_remaining%16); //for example if the space is 65 bytes we allocate 64
-                            current_record->next_record = (char *)current_record + sizeof(heap_record) + current_record->section_size;
-                            heap_record *new_record = current_record->next_record;
-                            new_record->next_record = (char *)current_record + sizeof(heap_record) + old_record_section_size;
-                            new_record->section_size = new_size;
-                            return (char *)current_record + sizeof(heap_record);
-                        }
-                    }
-                }
-            }
-            current_record = current_record->next_record;
-        }
-
-        if (current_record->free == 1){
-            // Check to see if the (last?) freed chunck is the right size
-            if (size <= current_record->section_size) {
-                //copy data from previous block to new block
-                memcpy(current_record, record_to_reallocate, sizeof(heap_record) + record_to_reallocate->section_size);
-                current_record->section_size = size;
-                current_record->free = 0;
-                record_to_reallocate->free=1;
-                return (char *)current_record + sizeof(heap_record);
-            }
-        }
-
-        // At this point, we have checked all existing records and have not found a free one that's big enough.
-        if ((void *) ((char *)current_record + calculate_nearest_size(size)) >= (void *) top_of_heap){
-            // We have reached the end of the heap, so we must extend it.
-            if (brk((char *) top_of_heap + extra_bytes) == -1){
-                return NULL;
-            }
-        }
-        // initialize fresh block and duplicate memory
-        void *new_record = malloc(size);
-        memcpy(new_record, record_to_reallocate, sizeof(heap_record) + record_to_reallocate->section_size);
-        //free previous block
-        record_to_reallocate->free = 1;
-        return new_record;
-    } else {
-        // The block to reallocate has enough space
-        int old_record_section_size = record_to_reallocate->section_size;
-        record_to_reallocate->section_size = size;
-
-        if(record_to_reallocate->next_record != record_to_reallocate) {
-            // try to see if we can fit a memory block between the reallocated block and the one after
-            int space_remaining = (char *)record_to_reallocate->next_record - ((char *)record_to_reallocate + sizeof(heap_record)*2 + record_to_reallocate->section_size);
-            //check if a new block can be allocated within remaining space
-            if(space_remaining >= 16) {
-                //create new block within remaining space
-                int new_size = space_remaining-(space_remaining%16); //for example if the space is 65 bytes we allocate 64
-                record_to_reallocate->next_record = (char *)record_to_reallocate + sizeof(heap_record) + record_to_reallocate->section_size;
-                heap_record *new_record = record_to_reallocate->next_record;
-                new_record->next_record = (char *)record_to_reallocate + sizeof(heap_record) + old_record_section_size;
-                new_record->section_size = new_size;
-                return (char *)record_to_reallocate + sizeof(heap_record);
-            }
-        }
-        return (char *)record_to_reallocate + sizeof(heap_record);
+    size_t round_size = calculate_nearest_size(size);
+    if (record_to_reallocate->section_size >= round_size){
+        return ptr;
+    }else{
+        void * new_chunk = malloc(size);
+        memcpy(new_chunk,ptr,record_to_reallocate->section_size);
+        free(ptr);
+        return new_chunk;
     }
 }
 
